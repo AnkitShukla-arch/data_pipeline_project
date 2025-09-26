5ef22c2cc6a6fbb9d603fc01283a75e0159310b3
Lee Christmas <monishukla727538@gmail.com>
2025-09-26 14:47:13 +0530

    Update audit_logger.py

--- logging_audit/audit_logger.py
+++ logging_audit/audit_logger.py
@@ -1,1 +1,21 @@
+import logging
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+logging.basicConfig(
+    filename="logs/pipeline.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
+
+def log_info(message: str):
+    logging.info(message)
+    print(f"✅ {message}")
+
+
+def log_error(message: str):
+    logging.error(message)
+    print(f"❌ {message}")
 
